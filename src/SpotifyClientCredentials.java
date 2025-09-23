871ec71b0fa4e3f159d1fcd0779c6444db7b88d0
llmon <levon.lau22@gmail.com>
2025-09-23 13:42:36 -0400

    Remove Spotify credentials from source code

--- src/SpotifyClientCredentials.java
+++ src/SpotifyClientCredentials.java
@@ -9,36 +9,9 @@ import java.util.Base64;
 
 public class SpotifyClientCredentials {
 
-    /*public static void main(String[] args) throws Exception {
-        String clientId = "***REMOVED***";
-        String clientSecret = "***REMOVED***";
-
-        // Encode client_id:client_secret in Base64
-        String credentials = Base64.getEncoder().encodeToString((clientId + ":" + clientSecret).getBytes());
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://accounts.spotify.com/api/token"))
-                .header("Authorization", "Basic " + credentials)
-                .header("Content-Type", "application/x-www-form-urlencoded")
-                .POST(HttpRequest.BodyPublishers.ofString("grant_type=client_credentials"))
-                .build();
-
-        HttpClient client = HttpClient.newHttpClient();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        System.out.println("Response: " + response.body());
-
-        JsonObject json = JsonParser.parseString(response.body()).getAsJsonObject();
-        String accessToken = json.get("access_token").getAsString();
-        int expiresIn = json.get("expires_in").getAsInt();
-
-        System.out.println("Access Token: " + accessToken);
-        System.out.println("Expires in: " + expiresIn + " seconds");
-    }
-    */
     public static String getAccessToken() throws Exception{
-    	String clientId = "***REMOVED***";
-        String clientSecret = "***REMOVED***";
+    	String clientId = System.getenv("SPOTIFY_CLIENT_ID");
+        String clientSecret = System.getenv("SPOTIFY_CLIENT_SECRET");
 
         // Encode client_id:client_secret in Base64
         String credentials = Base64.getEncoder().encodeToString((clientId + ":" + clientSecret).getBytes());
